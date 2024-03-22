55fa51d3f864f3476633020db48d6eca4797ced7
Alan <alan.wang@theia.tw>
2024-03-22 16:05:38 +0800

    Add the main function of analyzer

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import csv, argparse, logging
 
 CSV_PATH = ''
-
-COMMAND_ANALYZE = 'ana'
+RESULT_ATTRIBUTE = ['Total number of particles',
+                    'IM>0 & (CD63>0 OR CD81>0)',
+                    'IM>0 & (CD63>0 OR CD81>0) AND FITC>0',
+                    'IM>0 & (CD63>0 OR CD81>0) AND FITC=0',
+                    'IM>0 & (CD63=0 AND CD81=0)',
+                    'IM>0 & (CD63=0 AND CD81=0) AND FITC>0',
+                    'IM>0 & (CD63=0 AND CD81=0) AND FITC=0',
+                    'IM=0 & (CD63>0 OR CD81>0)',
+                    'IM=0 & (CD63>0 OR CD81>0) AND FITC>0',
+                    'IM=0 & (CD63>0 OR CD81>0) AND FITC=0',
+                    'IM=0 & (CD63=0 AND CD81=0)',
+                    'IM=0 & (CD63=0 AND CD81=0) AND FITC>0',
+                    'IM=0 & (CD63=0 AND CD81=0) AND FITC=0']
 
 
 # Gather our code in a main() function
@@ -12,14 +23,78 @@ def main(args, loglevel):
     logging.debug("Your Argument: %s" % args.argument)
 
     if '/' in args.argument:
-        print('\nstart setting the csv path', args.argument)
+        print('start analyzing the csv file', args.argument)
         with open(args.argument, newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            for row in spamreader:
-                print(', '.join(row))
-                break
+            data = csv.reader(csvfile, delimiter=',', skipinitialspace=True)
+            cnt = 1
+            antibody = []
+            result = [[0] * 13 for i in range(4)]
+            offset = 5
 
-        print('\nstart analyzing the csv file')
+            for row in data:
+                if cnt == 1:
+                    sample = row[0]
+                    # print('sample:', sample)
+                if cnt == 2:
+                    print('antibody:')
+                    antibody_name = ''
+                    for i in range(len(row)):
+                        if i % offset == 1:
+                            antibody.append(row[i])
+                            antibody_name += row[i] + ' '
+                    print(antibody_name)
+                if cnt == 3:
+                    print('channel:')
+                    print(row[1], row[2], row[3], row[4])
+
+                if cnt >= 4:
+                    for i in range(len(row) // 5):
+                        # i: 0 1 2 3
+                        # CD63 1, CD81 2, FITC 3, IM 4
+                        if row[i * 5 + 1] == '':
+                            continue
+                        cd63 = int(row[i * 5 + 1])
+                        cd81 = int(row[i * 5 + 2])
+                        fitc = int(row[i * 5 + 3])
+                        im = float(row[i * 5 + 4])
+
+                        if row[i * 5 + 1] != '':
+                            result[i][0] += 1
+                        if im > 0 and (cd63 > 0 or cd81 > 0):
+                            result[i][1] += 1
+                            if fitc > 0:
+                                result[i][2] += 1
+                            if fitc == 0:
+                                result[i][3] += 1
+                        if im > 0 and (cd63 == 0 and cd81 == 0):
+                            result[i][4] += 1
+                            if fitc > 0:
+                                result[i][5] += 1
+                            if fitc == 0:
+                                result[i][6] += 1
+                        if im == 0 and (cd63 > 0 or cd81 > 0):
+                            result[i][7] += 1
+                            if fitc > 0:
+                                result[i][8] += 1
+                            if fitc == 0:
+                                result[i][9] += 1
+                        if im == 0 and (cd63 == 0 and cd81 == 0):
+                            result[i][10] += 1
+                            if fitc > 0:
+                                result[i][11] += 1
+                            if fitc == 0:
+                                result[i][12] += 1
+                    # print(cnt, result[0])
+                cnt += 1
+
+        result_name = str(args.argument.split('.csv')[0]) + "_result.csv"
+        print('start organizing the result', result_name)
+        fp = open(result_name, "w")
+        for i in range(len(result)):
+            fp.write(antibody[i])
+            for j in range(len(result[i])):
+                fp.write(',' + RESULT_ATTRIBUTE[j] + ',' + str(result[i][j]) + '\n')
+        fp.close()
 
 
 # Standard boilerplate to call the main() function to begin
